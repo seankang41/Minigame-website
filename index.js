ef8d0339c8ba35248bd5d75704edde3a7e39d1e1
seankang41 <96400858+seankang41@users.noreply.github.com>
2022-11-25 13:32:12 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function startGameHang() {
+    window.location.href="file:///Users/seankang/Documents/javascript/Project%201/Publications/Hangman/hangman.html"
+
+}
+
+function startGameMath() {
+    window.location.href = "file:///Users/seankang/Documents/javascript/Project%201/Publications/Math%20Wiz/Math.html"
+}
+
+function startGameBlackJack() {
+    window.location.href = "file:///Users/seankang/Documents/javascript/Project%201/Publications/BlackJack/index.html"
+}
+
+function startDinosaurgame() {
+    window.location.href = "file:///Users/seankang/Documents/javascript/Project%201/Publications/Dinosaur%20game/index.html"
+}
